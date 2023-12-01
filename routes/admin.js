7c052d520d19d63c6cffb761d0fa1756c94363ca
Tanzeel <tanzeel498@gmail.com>
2023-12-02 00:55:05 +0500

    add css files and serve static files

--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,12 @@
 const express = require("express");
+const router = express.Router();
 const path = require("path");
 
-const router = express.Router();
+const rootDir = require("../util/rootPath");
 
 // reachable through /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
-  console.log("In product middleware!");
-  res.sendFile(path.join(__dirname, "../", "views", "add-product.html"));
+  res.sendFile(path.join(rootDir, "views", "add-product.html"));
 });
 // reachable through /admin/add-product => POST
 router.post("/add-product", (req, res, next) => {
