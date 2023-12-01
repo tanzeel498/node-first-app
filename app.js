544d6ca85443a02ed591e4dba8d9a4c009ebf9a7
Tanzeel <tanzeel498@gmail.com>
2023-12-01 16:51:18 +0500

    add middleware and body-parser

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-const http = require("node:http");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const server = http.createServer();
+const app = express();
 
-server.listen(3000);
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  console.log("In product middleware!");
+  res.send(
+    "<form method='POST' action='/product'><input type='text' name='title' /><button type='submit'>Add Product</button></form>"
+  );
+});
+
+app.post("/product", (req, res, next) => {
+  console.log(req.body);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In the middleware!");
+  res.send("<h1>Hello from Express</h1>");
+});
+
+app.listen(3000);
