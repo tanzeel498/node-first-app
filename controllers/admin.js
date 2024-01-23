6d606e3b4c6f31dc15c2680523ca980be1d469e5
Tanzeel <tanzeel498@gmail.com>
2024-01-24 00:18:12 +0500

    add editProduct and addProduct validation

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const { validationResult } = require("express-validator");
 
 exports.getAddProduct = (req, res, next) => {
   if (!req.session.isLoggedIn) {
@@ -8,6 +9,9 @@ exports.getAddProduct = (req, res, next) => {
     pageTitle: "Add Product",
     path: "/admin/add-product",
     editing: false,
+    hasError: false,
+    errorMessage: null,
+    validationErrors: [],
   });
 };
 
@@ -16,6 +20,21 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Add Product",
+      path: "/admin/edit-product",
+      editing: false,
+      hasError: true,
+      product: { title, imageUrl, price, description },
+      errorMessage: errors.array().at(0).msg,
+      validationErrors: errors.array(),
+    });
+  }
+
   const product = new Product({
     title: title,
     price: price,
@@ -51,6 +70,9 @@ exports.getEditProduct = (req, res, next) => {
         path: "/admin/edit-product",
         editing: editMode,
         product: product,
+        hasError: false,
+        errorMessage: null,
+        validationErrors: [],
       });
     })
     .catch((err) => console.log(err));
@@ -62,6 +84,26 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDesc = req.body.description;
+
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("admin/edit-product", {
+      pageTitle: "Edit Product",
+      path: "/admin/edit-product",
+      editing: true,
+      hasError: true,
+      product: {
+        title: updatedTitle,
+        imageUrl: updatedImageUrl,
+        price: updatedPrice,
+        description: updatedDesc,
+        _id: prodId,
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
+    });
+  }
 
   Product.findById(prodId)
     .then((product) => {
--- routes/admin.js
+++ routes/admin.js
@@ -1,14 +1,35 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
 
 const isAuth = require("../middleware/is-auth");
 const adminController = require("../controllers/admin");
 
 router.get("/products", isAuth, adminController.getProducts);
 router.get("/add-product", isAuth, adminController.getAddProduct);
-router.post("/add-product", isAuth, adminController.postAddProduct);
+router.post(
+  "/add-product",
+  [
+    body("title").isString().trim().rtrim().isLength({ min: 3 }),
+    body("imageUrl").isURL(),
+    body("price").isFloat(),
+    body("description").trim().rtrim().isLength({ min: 5, max: 400 }),
+  ],
+  isAuth,
+  adminController.postAddProduct
+);
 router.get("/edit-product/:productId", isAuth, adminController.getEditProduct);
-router.post("/edit-product", isAuth, adminController.postEditProduct);
+router.post(
+  "/edit-product",
+  [
+    body("title").isString().trim().rtrim().isLength({ min: 3 }),
+    body("imageUrl").isURL(),
+    body("price").isFloat(),
+    body("description").trim().rtrim().isLength({ min: 5, max: 400 }),
+  ],
+  isAuth,
+  adminController.postEditProduct
+);
 router.post("/delete-product", isAuth, adminController.postDeleteProduct);
 
 module.exports = router;
